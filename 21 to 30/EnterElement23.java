6203153b31bd80efc311ef7eee59b0a7c295c136
Krutarth Zala <krutarthzala2@gmail.com>
2023-03-29 00:08:11 +0530

    23rd Program half done

---
+++ 21 to 30/EnterElement23.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class EnterElement23 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size : ");
+        int arrSize = sc.nextInt();
+
+        
+        int [] numArr = new int[arrSize];
+        
+        for (int i = 0; i < arrSize; i++) {
+            System.out.print("Enter "+ (i+1) +" element : ");
+            numArr[i] = sc.nextInt();
+
+        }
+
+        System.out.print("Array before insert element : ");
+        printArr(numArr);
+        
+        System.out.print("Enter position : ");
+        int position = sc.nextInt();
+        
+        System.out.print("Enter element : ");
+        int element = sc.nextInt();
+
+
+        InsertElement ie = new InsertElement();
+        ie.InsertInArray(numArr, arrSize,position,element);
+
+
+        
+    }
+    static void printArr(int []numArr) {
+        for (int i = 0; i < numArr.length; i++) {
+            System.out.print(numArr[i] +" ");
+        }
+    }
+}
+
+class InsertElement
+{
+    public void InsertInArray(int[] arr, int size,int pos,int val)
+    {
+        size++;
+        int temp;
+
+        for (int i = 0; i < arr.length; i++) {
+            if ((i+1) == pos) {
+                
+                int loopvar = pos - (i+1);
+                while ((pos - (i+1) == loopvar)) {
+                    temp = arr[i+1];
+                    arr[i+1] = arr[i];
+                    arr[i] = temp;
+                    i++;
+                } //*This method is complex 
+            
+                arr[pos - 1] = val;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] +" ");
+        }
+
+    }
+}
---
+++ 21 to 30/tempCodeRunnerFile.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class tempCodeRunnerFile
+{
+    public static void main(String[] args) {
+        
+    }
+}
